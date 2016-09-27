c46a474d3d7e9931f2f38cdcc9e40956f04d6472
Henk Middendorp <henk@henks-mbp.wireless.hhs.nl>
2016-09-27 09:35:37 +0200

    Initial Commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Henk Middendorp on 27/09/16.
+//  Copyright © 2016 Henk Middendorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
